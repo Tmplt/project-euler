82cf224d2391f2b5ea04841f2db8736949669f7b
Tmplt <tmplt@dragons.rocks>
2019-01-06 01:57:24 +0100

    restructure repo, add README

---
+++ README.md
@@ -0,0 +1,8 @@
+Project Euler solutions
+===
+
+A collection of my solutions for Project Euler problems, in runnable Haskell --- simply invoke with `ghc`.
+
+---
+
+This is free and unencumbered software released into the public domain.
---
+++ haskell/p003.hs
@@ -0,0 +1,10 @@
+{- Solution to Project Euler problem 3.
+ -
+ - The prime factors of 13195 are 5, 7, 7, 13 and 29.
+ - What is the largest prime factor of the number 600851475143?
+ -}
+
+ problem = 600851475143
+ main = print (show ans)
+
+ ans = problem
